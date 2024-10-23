06b95d195a4e8e49310ee83c41c40bf609823aff
Melladmor <melladmor@gmail.com>
2024-10-23 18:31:17 +0400

    add theme configrations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "480px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      colors: {
+        background: {
+          light: "#ffffff",
+          dark: "#1a202c",
+        },
+        text: {
+          light: "#1a202c",
+          dark: "#ffffff",
+        },
+      },
+    },
   },
   plugins: [],
+  darkMode: "class",
 };
